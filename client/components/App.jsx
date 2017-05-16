7f1b8452baf1785a7df893982de22568462ab0f0
Don Smith <locksmithdon@gmail.com>
2017-05-17 04:51:45 +1200

    provide solution

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+
+import Home from './Home'
+import Nav from './Nav'
+import RankListing from './RankListing'
+import Rank from './Rank'
+import Species from './Species'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div className='app'>
+        <h1>Navigating the taxonomic ranks</h1>
+
+        <div className='container'>
+          <Route path='/' component={Nav} />
+          <div className='content'>
+            <Route exact path='/' component={Home} />
+            <Route path='/list/:rank' component={RankListing} />
+            <Route path='/rank/:rank/:name' component={Rank} />
+            <Route path='/species/:id' component={Species} />
+          </div>
+        </div>
+      </div>
+    </Router>
   )
 }
 
---
+++ client/components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import {capitalize} from '../utilities'
+import ranks from '../../data/ranks'
+
+const rankList = Object.keys(ranks)
+
+const Nav = ({location}) => {
+  return (
+    <div className='nav'>
+      <h2>Nav</h2>
+      <ul>
+        {rankList.map((rankName, key) => {
+          const normal = <li key={key}><Link to={`/list/${rankName}`}>{capitalize(rankName)}</Link></li>
+          const selected = <li key={key}><strong><Link to={`/list/${rankName}`}>{capitalize(rankName)}</Link></strong></li>
+          return location.pathname.indexOf(rankName) > 0 ? selected : normal
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
