94fcfa6dd2daade3d17e7f381fdae49e7eb7eff2
Caitlyn Bayley <caitlyn.bayley@gmail.com>
2018-06-20 11:31:32 +1200

    Nav and Home added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+import Nav from './Nav'
+import Home from './Home'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div>
+        <h1>Navigating the taxonomic ranks</h1>
+        <div className='Nav'>
+          <Nav/>
+        </div>
+        <div className='Home'>
+          <Home/>
+        </div>
+      </div>
+    </Router>
   )
 }
 
 export default App
-
