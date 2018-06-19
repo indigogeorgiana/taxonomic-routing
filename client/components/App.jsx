05fcf837d77148dcb29dd4e3f0784c09af443eda
Bronwyn Cotter <bron.cotter@icloud.com>
2018-06-20 10:55:36 +1200

    Basic home and App components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+import Home from '/Home'
+import Nav from '/Nav'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <h1>Navigating the Taxonomic Ranks</h1>
+
+      <Link to='/nav'>Nav</Link>
+      
+      <Route exact path = '/' component={Home} />
+      <Route path = '/nav' component={Nav} />
+
+    </Router>
   )
 }
 
 export default App
-
