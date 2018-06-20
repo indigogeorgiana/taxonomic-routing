121b9cba567c903c4e4a049bc43dd9c764826751
sam-by-name <sam-sanders@hotmail.co.uk>
2018-06-21 00:09:53 +1200

    ul added but not populated

--- client/components/NavBody.jsx
+++ client/components/NavBody.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import ranks from '../data/ranks'
+import species from '../data/species'
 
 const NavBody = props => {
   const title = props.match.params.rank
@@ -17,6 +18,17 @@ const NavBody = props => {
     <div className="homeCont">
       <h2>{name}</h2>
       <p>{description}</p>
+      <Link to={`/list/${rank}/species`}>Show species</Link>
+      <h3>Species</h3>
+      <ul>
+        {
+          species.map(specie => {
+            return (
+              <li key={specie.id}>{specie.title}</li>
+            )
+          })
+        }
+      </ul>
       <Link to='/'><p className="return">Go Home</p></Link>
     </div>
   )
