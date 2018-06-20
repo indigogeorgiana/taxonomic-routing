4c9928e14bf77a8fc5efb755958653d98f47d988
Indigo Phillips <indigogeorgiana@gmail.com>
2018-06-20 12:06:41 +1200

    Completed build of routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+import Home from './Home'
+import Nav from './Nav'
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+        <div>
+          <h1>Navigating the Taxonomic Ranks</h1>
+
+          <Link to='/nav'>Nav</Link> - {' '}
+          <Link to='/home'>Home</Link>
+
+          <Route exact path='/' component={Home} />
+          <Route path='/nav/' component={Nav} />
+
+          
+
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App
-
