00c952726544c6e3a72fe54f93b306505ab71910
ZeBin Xiao <juniorxiao01@gmail.com>
2018-06-20 17:47:47 +1200

    starting details jsx

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import ranks from '../../data/ranks'
 
-const List = (props) => {
-  const rankName = props.match.params.name
+const Details = (props) => {
+  const rankName = props.match.params.rank
+  const rankDesName = props.match.params.name
   const thisRanks = ranks[rankName]
+  console.log(rankDesName)
   return (
     <div>
       <h2>{rankName}</h2>
-      <ul>
+      <div>
         {
-          thisRanks.map(thisRank => {
-            return <li key={thisRank.name}> <Link to={`/rank/${rankName}/${thisRank.name}`}>{thisRank.name}</Link></li>
-          })
         }
-      </ul>
+      </div>
     </div>
   )
 }
 
-export default List
+export default Details
+
+// {
+//   thisRanks.map(thisRank => {
+//     return <li key={thisRank.name}> <Link to={`/rank/${rankName}/${thisRank.name}`}>{thisRank.name}</Link></li>
+//   })
+// }
